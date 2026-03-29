998bae4ddb459b1ad215ac813a3d0171a0c6f7e4
RoiGaon <roigaon@gmail.com>
2026-03-29 15:57:58 +0000

    Stupid errors on my part

--- runTrain.sh
+++ runTrain.sh
@@ -1,4 +1,4 @@
-!NUMBA_CUDA_USE_NVIDIA_BINDING=1 HYDRA_FULL_ERROR=1 python ./speech_to_text_finetune.py \
+NUMBA_CUDA_USE_NVIDIA_BINDING=1 HYDRA_FULL_ERROR=1 python ./speech_to_text_finetune.py \
   --config-path="." \
   --config-name="speech_to_text_finetune" \
   trainer.accumulate_grad_batches=16 \
